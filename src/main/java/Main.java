84c61119da03b6915944c43e2c4aa33b98856cc8
tb <buciortomasz@gmail.com>
2022-03-02 20:35:57 +0100

    Changes in repository. Added gitignore previously. Fixed naming conventions, added separate text class.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(Texts.invitation);
+        int rand = new Random().nextInt(100);
+        int tries = 5;
+        System.out.println(rand);
 
-    public static void main(String args[]) {
+        while (true) {
+            System.out.println(Texts.rules);
+            var input = new Scanner(System.in);
+            if (!input.hasNextInt()) {
+                System.out.println(Texts.thisIsSad + " " + input.next());
+                return;
+            }
+            var x = input.nextInt();
+            if (x > 99) {
+                System.out.println(Texts.numberIsGreater);
+            } else if (x < 0) {
+                System.out.println(Texts.numberIsLower);
+            }
 
+            if (x == rand) {
+                System.out.println(Texts.winner);
+                break;
+            } else {
+                System.out.println(Texts.tryAgain);
+                tries--;
+                System.out.println(Texts.triesLeftP1 + " " + tries + Texts.triesLeftP2);
+            }
+            if (tries == 0) {
+                System.out.println(Texts.oupsNotThisTime + " " + rand);
+                break;
+            }
+        }
     }
 }
+
+
---
+++ src/main/java/Texts.java
@@ -0,0 +1,12 @@
+public class Texts {
+    public static final String invitation = "Let's play a guessing game.";
+    public static final String rules = "Please provide a number between 0 to 99.";
+    public static final String numberIsGreater = "Your number is GREATER than the one you are trying to guess";
+    public static final String numberIsLower = "Your number is LOWER than the one you are trying to guess";
+    public static final String tryAgain = "Please try again.";
+    public static final String triesLeftP1 = "You have: ";
+    public static final String triesLeftP2 = " tries left.";
+    public static final String winner = "You guessed it!";
+    public static final String oupsNotThisTime = "Sorry you didn't guess the number, the answer was:";
+    public static final String thisIsSad = "That is not an integer.\nYou have inputted:";
+}
